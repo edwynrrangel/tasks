4bf459c87a0d296173f47f4bcc1350e0096af96f
Edwyn Rangel <edwynrangel@gmail.com>
2023-10-19 21:09:37 -0500

    added load env more getting postgresql connection

--- config/config.go
+++ config/config.go
@@ -1,13 +1,27 @@
 package config
 
-import "os"
+import (
+	"database/sql"
+	"fmt"
+	"os"
 
-type Configuration struct {
-	Port               string
-	CorsAllowedOrigins string
-}
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+
+	"github.com/edwynrrangel/tasks/logger"
+)
+
+type (
+	Configuration struct {
+		Port               string
+		CorsAllowedOrigins string
+		PostgreSQLClient   *sql.DB
+	}
+)
 
 func LoadConfig() *Configuration {
+	loadEnv()
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "3000"
@@ -18,10 +32,46 @@ func LoadConfig() *Configuration {
 		corsAllowedOrigins = "*"
 	}
 
+	clientDB, err := getPostgresqlConfig()
+	if err != nil {
+		logger.Fatal(err)
+	}
+
 	return &Configuration{
 		Port:               port,
 		CorsAllowedOrigins: corsAllowedOrigins,
+		PostgreSQLClient:   clientDB,
 	}
+}
+
+func loadEnv() {
+	if err := godotenv.Load(); err != nil {
+		logger.Warn("error loading .env file")
+	}
+}
+
+func getPostgresqlConfig() (*sql.DB, error) {
+	client, err := sql.Open(
+		"postgres",
+		fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+			os.Getenv("DB_HOST"),
+			os.Getenv("DB_PORT"),
+			os.Getenv("DB_USER"),
+			os.Getenv("DB_PASSWORD"),
+			os.Getenv("DB_NAME"),
+		),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	err = client.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	logger.Info("Connected to PostgreSQL!")
+	return client, nil
 }
 
 var Config *Configuration
