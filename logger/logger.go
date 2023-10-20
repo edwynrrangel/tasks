cce9bdc1192de8de2eec0feb74283803666649c4
Edwyn Rangel <edwynrangel@gmail.com>
2023-10-19 19:32:51 -0500

    added logger package

---
+++ logger/logger.go
@@ -0,0 +1,42 @@
+package logger
+
+import (
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+var logger *zap.Logger
+
+// init configure zap library
+func init() {
+	var err error
+
+	config := zap.NewProductionConfig()
+
+	encoderConfig := zap.NewProductionEncoderConfig()
+	encoderConfig.TimeKey = "timestamp"
+	encoderConfig.EncodeTime = zapcore.RFC3339TimeEncoder
+	encoderConfig.StacktraceKey = ""
+	config.EncoderConfig = encoderConfig
+
+	logger, err = config.Build(zap.AddCallerSkip(1))
+	if err != nil {
+		panic(err)
+	}
+}
+
+func Error(msg string, keysAndValues ...interface{}) {
+	logger.Sugar().Errorw(msg, keysAndValues...)
+}
+
+func Info(msg string, keysAndValues ...interface{}) {
+	logger.Sugar().Infow(msg, keysAndValues...)
+}
+
+func Warn(args ...interface{}) {
+	logger.Sugar().Warn(args...)
+}
+
+func Fatal(args ...interface{}) {
+	logger.Sugar().Fatal(args...)
+}
